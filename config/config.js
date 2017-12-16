72925ad1cb145738acbd8bbea9f0580c684c531f
Ken Huh <ken123777@gmail.com>
2017-12-16 21:09:57 +0900

    Update DB config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const rootPath = path.normalize(__dirname + '/..');
 const env = process.env.NODE_ENV || 'development';
-const DB_PW = process.env.DB_PW;
 
 const config = {
   development: {
@@ -10,7 +9,7 @@ const config = {
       name: 'vanilla-study-development'
     },
     port: process.env.PORT || 3000,
-    db: `mongodb://Vanilla:${DB_PW}@development-shard-00-00-bqwt8.mongodb.net:27017,development-shard-00-01-bqwt8.mongodb.net:27017,development-shard-00-02-bqwt8.mongodb.net:27017/admin?replicaSet=development-shard-0&ssl=true`
+    db: `mongodb://vanilla:vanilla@ds059546.mlab.com:59546/vanilla-study`
   },
 
   production: {
@@ -19,8 +18,11 @@ const config = {
       name: 'vanilla-study-production'
     },
     port: process.env.PORT || 3000,
-    db: `mongodb://Vanilla:${DB_PW}@production-shard-00-00-alokq.mongodb.net:27017,production-shard-00-01-alokq.mongodb.net:27017,production-shard-00-02-alokq.mongodb.net:27017/admin?replicaSet=production-shard-0&ssl=true`
+    db: `mongodb://vanilla:vanilla@ds159676.mlab.com:59676/vanilla-study-development`
   }
 };
 
 module.exports = config[env];
+
+
+vaN1LlacOd!nG
